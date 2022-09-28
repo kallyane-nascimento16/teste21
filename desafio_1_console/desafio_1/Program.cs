6981b56ff1a177b5f1624b0c33c4d4a7b49e25b1
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:45:43 -0300

    Add files via upload

---
+++ desafio_1_console/desafio_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_1_console
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10;00");
+            for (int min = 9; min >= 0; min = min - 1)
+            {
+                for (int seg = 59; seg >= 0; seg = seg - 1)
+                {
+
+                    Console.WriteLine(min + ":" + seg);
+                   
+                }
+                
+
+            }
+
+        }
+    }
+}
